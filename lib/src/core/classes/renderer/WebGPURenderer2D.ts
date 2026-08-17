5ba4e8ecafeebe77063de0dcd53fcd83efb19aac
Rohan <tr.kanhaisingh@gmail.com>
2026-08-17 11:58:49 +0200

    First WebGPURenderer2D implementation.

--- lib/src/core/classes/renderer/WebGPURenderer2D.ts
+++ lib/src/core/classes/renderer/WebGPURenderer2D.ts
@@ -1,7 +1,20 @@
 import { v4 } from "uuid";
+import { Canvas } from "./Canvas";
 
 export class WebGPURenderer2D {
-    
+
     public id: string = v4();
+
+    public canvas: Canvas | null = null;
+    public clearColor: GPUColorDict | null = null;
+
+    private adapter: GPUAdapter | null = null;
+    private device: GPUDevice | null = null;
+    private context: GPUCanvasContext | null = null;
+    private presentationFormat: GPUTextureFormat = "bgra8unorm";
+
+    private initialized: boolean = false;
+
     
+
 }
